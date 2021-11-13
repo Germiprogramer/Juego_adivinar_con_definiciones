3587eed29a0e87e1414fc153f7890b9ecad31131
Germiprogramer <llorentemunozgerman@gmail.com>
2021-11-13 18:42:35 +0100

    Update JUEGODEADIVINARCONDEF.py

--- JUEGODEADIVINARCONDEF.py
+++ JUEGODEADIVINARCONDEF.py
@@ -5,26 +5,46 @@ while True:
     nivel = input("Introduce un nivel: simple, intermedio, avanzado, experto, o Stephen Curry:  ")
     if nivel == "simple":
         MAX = 100
+        numero_intentos = 15
         break
     elif nivel == "intermedio":
         MAX = 1000
+        numero_intentos = 55
         break
     elif nivel == "avanzado":
         MAX = 1000000
+        numero_intentos = 105
         break
     elif nivel == "experto":
         MAX = 1000000000000
+        numero_intentos = 155
         break
     elif nivel == "Stephen Curry":
         MAX = 10000000000000000000000000000000000000000000000
+        numero_intentos = 100000000000000000
         print("SUERTE CON LOS TRIPLES")
         break
     else:
         print("Como no has elegido te pongo el fácil pesao")
         break
 
+def help():
+    ayuda = input(print("¿Quieres una ayudita ;)? (Para ayudita pulse 1)"))
+    try:
+        if ayuda == "1":
+            print("Lo sentimos todavía no sabemos programar eso pero quería demostrarle a mi profe que entiendo funciones")
+        else:
+            print("pues nada...")
+    except:
+        pass
+    return ayuda
+
+help()
+
 minimo = MIN
 maximo = MAX
+import random
+numero = random.randint(minimo,maximo)
 
 def pedir_numero(invitacion, minimo, maximo):
     invitacion += " entre " + str(minimo) + " y " + str(maximo) + ": "
@@ -39,25 +59,22 @@ def pedir_numero(invitacion, minimo, maximo):
                 break
     return entrada
 
-import random
-numero = random.randint(0,100)
 contador = 1
 
-print("Intente adivinar el numero:")
 while True:
     intento = pedir_numero("Adivine el numero", minimo, maximo,)
     if intento < numero:
         print("Demasiado pequeñito :(")
         minimo = intento + 1
         contador = contador + 1
-        if contador > (maximo/10):
+        if contador > numero_intentos:
             print("TE HAS PASADO DEL NUMERO DE INTENTOS")
             break
     elif intento > numero:
         print("Demasiado grande")
         maximo = intento - 1
         contador = contador + 1
-        if contador > (maximo/10):
+        if contador > numero_intentos:
             print("TE HAS PASADO DEL NUMERO DE INTENTOS")
             break
     else:
